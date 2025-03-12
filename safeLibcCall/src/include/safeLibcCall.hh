dbb5dfe506b66c91be01597544927bb5db7110af
allelomorph <samuel.pomeroy@atlasschool.com>
2025-03-12 08:41:45 +0000

    safeLibcCall: first draft of in-source docs

--- safeLibcCall/src/include/safeLibcCall.hh
+++ safeLibcCall/src/include/safeLibcCall.hh
@@ -1,6 +1,9 @@
 #ifndef SAFELIBCCALL_HH
 #define SAFELIBCCALL_HH
 
+/**
+ * @file safeLibcCall.hh
+ */
 
 #include <functional>
 #include <stdexcept>     // runtime_error
@@ -10,25 +13,69 @@
 #include <type_traits>   // invoke_result_t
 
 
-/*
- * Using child classes instead of aliases due to need to differentiate testing
- *   only errno (int) vs only an int return value.
+/**
+ * @file safeLibcCall.hh
+ *
+ * @note
+ * [LibcRetErrTest](#LibcRetErrTest), [LibcRetTest](#LibcRetTest), and
+ *   [LibcErrTest](#LibcErrTest) made children of std::function instead of
+ *   aliases due to need to differentiate testing only errno (int) vs only an
+ *   int return value.
  * If performace becomes an issue similar aliases to function pointer types
  *   would work, but at the cost of making the errno/int return differentiation,
- *   and being able to pass in functors. For comparison of std::function vs
- *   function pointers: https://stackoverflow.com/q/25848690
+ *   and being able to pass in functors.
  * Note that when instantiating these types with a lambda, ReturnType must
- *   be passed as a template parameter to allow its deduction in safeLibcCall
+ *   be passed as a template parameter to allow its deduction in safeLibcCall.
+ *
+ * @see [Comparison of std::function vs function pointers](https://stackoverflow.com/q/25848690)
+ */
+
+/**
+ * @brief Allows passing of user-defined test of glibc function failure
+ *   condition (either by return or errno values) into safeLibcCall.
+ *
+ * @tparam ReturnType return type of glibc function
+ *
+ * @see [safeLibcCall](#safeLibcCall)
  */
 template<typename ReturnType>
 class LibcRetErrTest : public std::function<bool(const ReturnType, const int)> {};
 
+/**
+ * @brief Allows passing of user-defined test of glibc function failure
+ *   condition (by return value only) into safeLibcCall.
+ *
+ * @tparam ReturnType return type of glibc function
+ *
+ * @see [safeLibcCall](#safeLibcCall)
+ */
 template<typename ReturnType>
 class LibcRetTest : public std::function<bool(const ReturnType)> {};
 
+/**
+ * @brief Allows passing of user-defined test of glibc function failure
+ *   condition (by errno value only) into safeLibcCall.
+ *
+ * @see [safeLibcCall](#safeLibcCall)
+ */
 class LibcErrTest : public std::function<bool(const int)> {};
 
 
+/**
+ * @brief Wraps a glibc function call, throwing a descriptive exception when
+ *   the function fails (when detecting by return or errno values).
+ *
+ * @tparam FuncType prototype of glibc function
+ * @tparam ReturnType return type of glibc function
+ * @tparam ParamType parameter pack of glibc function parameter types
+ *
+ * @return return value of glibc function
+ *
+ * @throw std::runtime_error "(libc_func_name): failure without setting errno"
+ *   in case of failure by return value
+ * @throw std::system_error "(libc_func_name): (system error string for errno)"
+ *   in case of failure by errno (eg error string is usually std::strerror(errno))
+*/
 template<typename FuncType, typename ReturnType, typename ...ParamTypes>
 ReturnType safeLibcCall(const FuncType&& libc_func,
                         const std::string_view& libc_func_name,
@@ -53,6 +100,21 @@ ReturnType safeLibcCall(const FuncType&& libc_func,
     return retval;
 }
 
+/**
+ * @brief Wraps a glibc function call, throwing a descriptive exception when
+ *   the function fails (when detecting only by return value).
+ *
+ * @tparam FuncType prototype of glibc function
+ * @tparam ReturnType return type of glibc function
+ * @tparam ParamType parameter pack of glibc function parameter types
+ *
+ * @return return value of glibc function
+ *
+ * @throw std::runtime_error "(libc_func_name): failure without setting errno"
+ *   in case of failure by return value
+ * @throw std::system_error "(libc_func_name): (system error string for errno)"
+ *   in case of failure by errno (eg error string is usually std::strerror(errno))
+ */
 template<typename FuncType, typename ReturnType, typename ...ParamTypes>
 ReturnType safeLibcCall(const FuncType&& libc_func,
                         const std::string_view& libc_func_name,
@@ -77,6 +139,20 @@ ReturnType safeLibcCall(const FuncType&& libc_func,
     return retval;
 }
 
+/**
+ * @brief Wraps a glibc function call, throwing a descriptive exception when
+ *   the function fails (when detecting only by arbitrary errno value).
+ *
+ * @tparam FuncType prototype of glibc function
+ * @tparam ParamType parameter pack of glibc function parameter types
+ *
+ * @return return value of glibc function
+ *
+ * @throw std::runtime_error "(libc_func_name): failure without setting errno"
+ *   in case of failure with errno of 0
+ * @throw std::system_error "(libc_func_name): (system error string for errno)"
+ *   in case of failure by errno (eg error string is usually std::strerror(errno))
+ */
 template<typename FuncType, typename ...ParamTypes>
 auto safeLibcCall(const FuncType&& libc_func,
                   const std::string_view& libc_func_name,
@@ -102,6 +178,18 @@ auto safeLibcCall(const FuncType&& libc_func,
     return retval;
 }
 
+/**
+ * @brief Wraps a glibc function call, throwing a descriptive exception when
+ *   the function fails (when detecting only by non-zero errno value).
+ *
+ * @tparam FuncType prototype of glibc function
+ * @tparam ParamType parameter pack of glibc function parameter types
+ *
+ * @return return value of glibc function
+ *
+ * @throw std::system_error "(libc_func_name): (system error string for errno)"
+ *   in case of failure by errno (eg error string is usually std::strerror(errno))
+ */
 template<typename FuncType, typename ...ParamTypes>
 auto safeLibcCall(const FuncType&& libc_func,
                   const std::string_view& libc_func_name,
